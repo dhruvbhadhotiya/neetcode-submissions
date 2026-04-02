66f6080d2eaf8da95f2d42738b82871dd09657c7
Dhruv Bhadhotiya <dhruvbhadhotiya@gmail.com>
2026-04-02 15:37:44 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for(int i : nums) res ^= i;
+        for(int i = 0; i<= nums.length; i++) res ^= i;
+        return res;
+    }
+}
